ef9989da82698f8044a487ad5513bc7114177929
Imaadh <imaadhifthikar123@gmail.com>
2023-06-17 14:19:07 +0530

    Update urls.py

--- Calculator/urls.py
+++ Calculator/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import re_path, include
+from django.urls import re_path, include
 
 urlpatterns = [
     url(r'^$', include('calc.urls')),
